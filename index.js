6c783881102e56dcbae5abf975d6e194ca5d9e8a
scott-a-hall <hallsa3@yahoo.com>
2020-07-06 09:31:01 -0500

    update js files to include security/validation features

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express"),
   bodyParser = require("body-parser"),
   uuid = require("uuid");
+  cors = require("cors");
+
+const  { check, validationResult } = require("express-validator");
 
 const app = express();
 
@@ -16,11 +19,22 @@ mongoose.connect("mongodb://localhost:27017/myFlixDB", { useNewUrlParser: true, 
 
 //Middleware
 app.use(bodyParser.json());
-let auth = require('./auth')(app);
+let auth = require("./auth")(app);
+let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ //If a specific origin isn't found on the list of allowed allowedOrigins
+      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //Integrate passport
-const passport = require('passport');
-require('./passport');
+const passport = require("passport");
+require("./passport");
 
 let myLogger = (req, res, next) => {
   console.log(req.url);
@@ -114,15 +128,30 @@ app.get("/users/:Username", passport.authenticate('jwt', { session: false}), (re
   Email: String,
   Birthday: Date
 }*/
-app.post("/users", (req, res) => {
-  Users.findOne({ Username: req.body.Username })
+app.post("/users", [
+  check('Username', 'Username is required').isLength({min: 5}),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail(),
+], (req, res) => {
+
+  //check the validation object for errors
+  let errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  Users.findOne({ Username: req.body.Username }) //Search to see if a user with the requested username already exists
     .then((user) => {
       if (user) {
+        //If the user is found, return a response that it already exists
         return res.status(400).send(req.body.Username + ' already exists.');
       } else {
         Users.create({
             Username: req.body.Username,
-            Password: req.body.Password,
+            Password: hashedPassword,
             Email: req.body.Email,
             Birthday: req.body.Birthday
           })
@@ -149,12 +178,26 @@ app.post("/users", (req, res) => {
   Email: String, (required)
   Birthday: Date
 }*/
-app.put("/users/:Username", passport.authenticate('jwt', { session: false}), (req, res) => {
+app.put("/users/:Username", passport.authenticate('jwt', { session: false}), [
+  check('Username', 'Username is required').isLength({min: 5}),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail(),
+], (req, res) => {
+
+  //check the validation object for errors
+  let errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOneAndUpdate({ Username: req.params.Username },
     { $set:
       {
         Username: req.body.Username,
-        Password: req.body.Password,
+        Password: hashedPassword,
         Email: req.body.Email,
         Birthday: req.body.Birthday
       }
--- models.js
+++ models.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+  bcrypt = require("bcrypt");
 
 //Set movieSchema
 let movieSchema = mongoose.Schema({
@@ -25,6 +26,14 @@ let userSchema = mongoose.Schema({
   Birthday: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
