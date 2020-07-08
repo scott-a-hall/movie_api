57a4bb32df6126531bb578cae188a4dfb3af4363
scott-a-hall <hallsa3@yahoo.com>
2020-07-08 10:06:08 -0500

    enable cors for local react app domain

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifie
 //Middleware
 app.use(bodyParser.json());
 let auth = require("./auth")(app);
-let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+let allowedOrigins = ["http://localhost:8080", "http://localhost:1234"];
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
