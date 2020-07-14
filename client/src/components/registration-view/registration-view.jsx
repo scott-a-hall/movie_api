f34272a9a7916d473bb51efa3cff160c1d9adf39
scott-a-hall <hallsa3@yahoo.com>
2020-07-14 13:08:20 -0500

    update registration-view.jsx to include controlId for Email and Birthday

--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -17,19 +17,19 @@ export function RegistrationView(props) {
         <Form className="registration-form">
             <Form.Group controlId="formBasicUsername">
                 <Form.Label>Username</Form.Label>
-                <Form.Control type="Username" placeholder="Enter username" value={Username} />
+                <Form.Control type="Username" placeholder="Enter username" value={Username} onChange={e => createUsername(e.target.value)}/>
             </Form.Group>
             <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="Password" placeholder="Enter password" value={Password} />
+                <Form.Control type="Password" placeholder="Enter password" value={Password} onChange={e => createPassword(e.target.value)}/>
             </Form.Group>
-            <Form.Group>
+            <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="Email" placeholder="Enter valid email address" value={Email} />
+                <Form.Control type="Email" placeholder="Enter valid email address" value={Email} onChange={e => createEmail(e.target.value)}/>
             </Form.Group>
-            <Form.Group>
+            <Form.Group controlId="formBasicBirthday">
                 <Form.Label>Birthday</Form.Label>
-                <Form.Control type="date" placeholder="MM/DD/YYYY" value={Birthday} />
+                <Form.Control type="date" placeholder="MM/DD/YYYY" value={Birthday} onChange={e => createBirthday(e.target.value)}/>
             </Form.Group>
             <Button type="submit" onClick={handleSubmit}>Register</Button>
         </Form>
