cbb0ba8161fe99053cd38c711d894ab85c554f19
scott-a-hall <hallsa3@yahoo.com>
2020-07-17 10:05:46 -0500

    allow all origins to index.js file

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -36600,7 +36600,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59009" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61417" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
