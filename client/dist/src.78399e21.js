cc8240bc3f9629252b0c90365266209860d5110c
scott-a-hall <hallsa3@yahoo.com>
2020-07-20 08:17:16 -0500

    add @types to express, body-parser, uuid, cors, mongoose dependencies

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -36604,7 +36604,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65005" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65523" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
