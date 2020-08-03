1fe3c3473f1656785a9378497edac7634bb55569
scott-a-hall <hallsa3@yahoo.com>
2020-08-03 13:23:54 -0500

    update index.jsx to include store from redux

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,36 @@
+//new code incorporating the store feature
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import MainView from './components/main-view/main-view';
+import moviesApp from './reducers/reducers';
+
+//Import statement to indicate that you need to bundle './index.scss'
+import './index.scss';
+
+const store = createStore(moviesApp);
+
+//Main component (will eventually use all the others)
+class MyFlixApplication extends React.Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <MainView />
+            </Provider>
+        );
+    }
+}
+
+//Finds the root of your app
+const container = document.getElementsByClassName('app-container')[0];
+
+//Tells React to render your app in the root DOM element
+ReactDOM.render(React.createElement(MyFlixApplication), container);
+
+//old code
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { MainView } from './components/main-view/main-view';
@@ -17,4 +49,4 @@ class MyFlixApplication extends React.Component {
 const container = document.getElementsByClassName('app-container')[0];
 
 //Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApplication), container);
+ReactDOM.render(React.createElement(MyFlixApplication), container);*/
