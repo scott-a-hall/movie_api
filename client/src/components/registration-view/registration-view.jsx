f6d764a94f9a03eeec18d561b3249c86e984fdaa
hallsa3 <62564304+hallsa3@users.noreply.github.com>
2020-08-07 07:49:31 -0500

    add link to login page

--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -2,15 +2,16 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
 import './registration-view.scss';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
 export function RegistrationView(props) {
-    const [ username, createUsername ] = useState ('');
-    const [ password, createPassword ] = useState ('');
-    const [ email, createEmail ] = useState('');
-    const [ birthday, createBirthday ] = useState('');
+    const [username, createUsername] = useState('');
+    const [password, createPassword] = useState('');
+    const [email, createEmail] = useState('');
+    const [birthday, createBirthday] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -20,37 +21,39 @@ export function RegistrationView(props) {
             Email: email,
             Birthday: birthday
         })
-        .then(response => {
-            const data = response.data;
-            console.log(data);
-            window.open('/','_self');
-        })
-        .catch(e => {
-            console.log('Error registering the user')
-        });
+            .then(response => {
+                const data = response.data;
+                console.log(data);
+                window.open('/', '_self');
+            })
+            .catch(e => {
+                console.log('Error registering the user')
+            });
     }
 
     return (
         <Container className="registration-form">
-        <Form>
-            <Form.Group controlId="formBasicUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="username" placeholder="Enter username" value={username} onChange={e => createUsername(e.target.value)}/>
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter password" value={password} onChange={e => createPassword(e.target.value)}/>
-            </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter valid email address" value={email} onChange={e => createEmail(e.target.value)}/>
-            </Form.Group>
-            <Form.Group controlId="formBasicBirthday">
-                <Form.Label>Birthday</Form.Label>
-                <Form.Control type="date" placeholder="MM/DD/YYYY" value={birthday} onChange={e => createBirthday(e.target.value)}/>
-            </Form.Group>
-            <Button type="submit" onClick={handleSubmit}>Register</Button>
-        </Form>
+            <Form>
+                <Form.Group controlId="formBasicUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="username" placeholder="Enter username" value={username} onChange={e => createUsername(e.target.value)} />
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Enter password" value={password} onChange={e => createPassword(e.target.value)} />
+                </Form.Group>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Enter valid email address" value={email} onChange={e => createEmail(e.target.value)} />
+                </Form.Group>
+                <Form.Group controlId="formBasicBirthday">
+                    <Form.Label>Birthday</Form.Label>
+                    <Form.Control type="date" placeholder="MM/DD/YYYY" value={birthday} onChange={e => createBirthday(e.target.value)} />
+                </Form.Group>
+                <Link to={`/login`}>
+                    <Button type="submit" onClick={handleSubmit}>Register</Button>
+                </Link>
+            </Form>
         </Container>
     );
 };
